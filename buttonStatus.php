e137c7de01aede605dd1a72157552f6d873bb707
kary7124 <puri_hema12@yahoo.com>
2017-04-20 13:37:01 +0530

    Update buttonStatus.php

--- buttonStatus.php
+++ buttonStatus.php
@@ -17,11 +17,12 @@ SELECT on_off FROM lightStatus ORDER BY id desc LIMIT 1 " ;//write here the valu
 $bla = $con->query($status);
 if ($bla->num_rows > 0) {
     // output data of each row
-    while($row = $result->fetch_assoc()) {
+    while($row = $bla>fetch_assoc()) {
         echo $row["on_off"];
     }
 } else {
     echo "0 results";
 }
 echo $bla;
+$con->close ();
 ?>
